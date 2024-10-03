2fa5614c07abef130afe02bb0b67767577819883
Ujjwal1220 <mujjwal769@gmail.com>
2024-10-03 12:49:22 +0530

    Handling multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,9 +2,28 @@ const express = require("express");
 
 const app = express(); //it will create the instance of express.,important line .
 
-app.get("/ab?c", (req, res) => {
-  res.send("Home Sweet Home");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1st response");
+    // res.send("1st Response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("2nd response");
+    // res.send("2nd response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("3rd response");
+    // res.send("3rd response");
+    next();
+  },
+  (req, res, next) => {
+    console.log("4th response");
+    res.send("4th response");
+  }
+);
 
 app.listen(7777, () => {
   console.log("Hey!");
